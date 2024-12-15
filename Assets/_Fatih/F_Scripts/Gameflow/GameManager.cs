9c1abc74e119f162ae019f923192acdf8afdfef0
Muhammet Fatih Aktaş <faatihaktaas@gmail.com>
2024-12-15 17:08:04 +0300

    added music source and settings

--- Assets/_Fatih/F_Scripts/Gameflow/GameManager.cs
+++ Assets/_Fatih/F_Scripts/Gameflow/GameManager.cs
@@ -8,11 +8,14 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] Slider sensSlider;
     [SerializeField] TMP_Text sensText;
+    [SerializeField] Slider musicSlider;
+    [SerializeField] TMP_Text musicVolumeText;
     [SerializeField] GameObject inGameSettingsPanel;
     [SerializeField] GameObject finalPanel;
     [SerializeField] TMP_Text finalPanelText;
     [SerializeField] bool pausePanel;
     [SerializeField] bool gameFinished;
+    [SerializeField] AudioSource musicSource;
 
     PlayerController _playerController;
 
@@ -27,6 +30,12 @@ public class GameManager : MonoBehaviour
         {
             sensSlider.value = PlayerPrefs.GetFloat("Sensivity");
             SetSensivityInGame();
+        }
+
+        if (PlayerPrefs.HasKey("Music"))
+        {
+            musicSlider.value = PlayerPrefs.GetFloat("Music");
+            SetMusicVolumeInGame();
         }
     }
 
@@ -57,6 +66,13 @@ public class GameManager : MonoBehaviour
         _playerController.SetMouseSensivity(sensSlider.value);
         PlayerPrefs.SetFloat("Sensivity", sensSlider.value);
         sensText.text = (sensSlider.value / 30f).ToString("N1");
+    }
+
+    public void SetMusicVolumeInGame()
+    {
+        PlayerPrefs.SetFloat("Music", musicSlider.value);
+        musicVolumeText.text = (musicSlider.value * 100).ToString();
+        musicSource.volume = musicSlider.value;
     }
 
     public void FinishTheGame(bool didPlayerWin)
--- Assets/_Fatih/F_Scripts/Menu/MenuManager.cs
+++ Assets/_Fatih/F_Scripts/Menu/MenuManager.cs
@@ -6,6 +6,9 @@ public class MenuManager : MonoBehaviour
 {
     [SerializeField] Slider sensivitySlider;
     [SerializeField] TMP_Text sensText;
+    [SerializeField] Slider musicSlider;
+    [SerializeField] TMP_Text musicVolumeText;
+    [SerializeField] AudioSource musicSource;
 
     void Start()
     {
@@ -18,12 +21,30 @@ public class MenuManager : MonoBehaviour
             SetSensivityInMenu();
             sensivitySlider.value = PlayerPrefs.GetFloat("Sensivity");
         }
+
+        if (PlayerPrefs.HasKey("Music"))
+        {
+            musicSlider.value = PlayerPrefs.GetFloat("Music");
+            musicSource.volume = PlayerPrefs.GetFloat("Music");
+        }
+        else
+        {
+            SetMusicVolumeInMenu();
+            musicSlider.value = PlayerPrefs.GetFloat("Music");
+        }
     }
 
     public void SetSensivityInMenu()
     {
         PlayerPrefs.SetFloat("Sensivity", sensivitySlider.value);
         sensText.text = (sensivitySlider.value / 30f).ToString("N1");
+    }
+
+    public void SetMusicVolumeInMenu()
+    {
+        PlayerPrefs.SetFloat("Music", musicSlider.value);
+        musicVolumeText.text = (musicSlider.value * 100).ToString();
+        musicSource.volume = musicSlider.value;
     }
 
 }
